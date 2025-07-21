87b43d67d2550250a4d303df65d5a67d01a1b91c
Om Patil <omspatil980@gmail.com>
2025-07-22 01:15:36 +0530

    Digital ocean deploy

--- app.js
+++ app.js
@@ -23,18 +23,18 @@ const corsOptions = {
 app.use(cors(corsOptions));
 app.options('*', cors(corsOptions)); // Handle preflight requests
 
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", process.env.FRONTEND_URL);
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  res.setHeader("Access-Control-Allow-Credentials", "true");
+// app.use((req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Origin", process.env.FRONTEND_URL);
+//   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+//   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+//   res.setHeader("Access-Control-Allow-Credentials", "true");
 
-  if (req.method === "OPTIONS") {
-    return res.sendStatus(200); // ✅ Respond to preflight requests
-  }
+//   if (req.method === "OPTIONS") {
+//     return res.sendStatus(200); // ✅ Respond to preflight requests
+//   }
 
-  next();
-});
+//   next();
+// });
 
 // app.use(cors());
 
